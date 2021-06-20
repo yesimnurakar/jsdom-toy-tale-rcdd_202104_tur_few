01cfd573dcb6a050310da0b09b713ec4389a4caf
Yesim nur Akar <yesimnurakar@gmail.com>
2021-06-20 14:12:06 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App.js";
+import App from "./App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));const addBtn = document.querySelector('#new-toy-btn')const toyForm = document.querySelector('.container')let addToy = falselet divCollect = document.querySelector('#toy-collection')
